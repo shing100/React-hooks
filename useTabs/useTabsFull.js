42d67befec86cdf980ca2c30d3ec65253411bf0d
DESKTOP-CN9OS9T\shing <shing100@naver.com>
2019-07-08 01:02:57 +0900

    useTabs

---
+++ useTabs/useTabsFull.js
@@ -0,0 +1,42 @@
+import React, { useState } from "react";
+import ReactDOM from "react-dom";
+
+import "./styles.css";
+
+const content = [
+    {
+        tab: "Section 1",
+        content: "I'm the content of the Section 1"
+    },
+    {
+        tab: "Section 2",
+        content: "I'm the content of the Section 2"
+    }
+];
+
+const useTabs = (initialTab, allTabs) => {
+    if (!allTabs || !Array.isArray(allTabs)) {
+        return;
+    }
+    const [currentIndex, setCurrentIndex] = useState(initialTab);
+    return {
+        currentItem: allTabs[currentIndex],
+        changeItem: setCurrentIndex
+    };
+};
+
+function App() {
+    const { currentItem, changeItem } = useTabs(0, content);
+
+    return (
+        <div className="App">
+            {content.map((section, index) => (
+                <button onClick={() => changeItem(index)}>{section.tab}</button>
+            ))}
+            <div>{currentItem.content}</div>
+        </div>
+    );
+}
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
